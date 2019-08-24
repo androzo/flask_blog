0eacdbd1a93c9fc82a3c272c766cc5461aa1e3d6
Andre Canabrava Buchmann <androzo@mac-pro.local>
2019-08-24 03:10:37 -0300

    new: login and register pages

--- src/flaskblog.py
+++ src/flaskblog.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, url_for
-#import socket
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 import connection
 
-# Connect to MySQL
-#db_host = socket.gethostbyname('database')
+""" # Connect to MySQL
 conn = connection.connect("database","sys","root","test")
 
 # Disconnect from MySQL
@@ -12,8 +11,10 @@ if conn is not None:
 else:
     print("Flask startup aborted")
     exit
-
+ """
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '357bf5222c435a949b9c2ccc7a82e914'
 
 posts = [
     {
@@ -38,6 +39,26 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title="About")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
 	app.run("0.0.0.0", port=80, debug=True)
